8cdf4093e23dff1bc47241f3219780048bfa8f1d
Prajwal Joshi <prajwal.j@codilar.com>
2023-08-05 15:40:02 +0530

    Initial commit for module

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,17 +2,33 @@
 
 namespace Codilar\B2bSpace\Controller\Index;
 
+use Magento\Company\Model\CompanyContext;
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Forward;
+use Magento\Framework\Controller\Result\ForwardFactory;
+use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Company\Model\CompanyContext;
-use Magento\Framework\Controller\Result\ForwardFactory;
 
 class Index implements ActionInterface
 {
+
+    /**
+     * @var PageFactory
+     */
     private PageFactory $resultPageFactory;
+
+    /**
+     * @var CompanyContext
+     */
     private CompanyContext $companyContext;
+
+    /**
+     * @var ForwardFactory
+     */
     private ForwardFactory $forwardFactory;
 
     /** Index Constructor
@@ -31,8 +47,9 @@ class Index implements ActionInterface
     }
 
     /**
-     * @throws NoSuchEntityException
+     * @return ResponseInterface|Forward|ResultInterface|Page
      * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function execute()
     {
